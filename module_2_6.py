0f88ee247719f32a7fc8de8f6c1f140cd4108e0b
Andey Melnikov <melnikov2010@yandex.ru>
2024-09-07 07:56:58 +0300

    Задание "Слишком древний шифр".

--- module_2_6.py
+++ module_2_6.py
@@ -7,9 +7,12 @@ def password(first_stone):
                 second_stone.append(code)
     return second_stone
 
-first_stone = int(input("Ввведите число, которое выпало на первом камне :"))
+first_stone = int(input("Число из первой вставки :"))
 while  first_stone < 3 or first_stone  > 20:
     first_stone = int(input("Посмотри ВНИМАТЕЛЬНО на число, которое выпало на первом камне :"))
 sec_code=password(first_stone)
-print("Введите следую последовательность для разблокировки ворот:"," - ", *sec_code)
+code_in_line=''
+for i in sec_code:
+    code_in_line+=str(i)
+print("Нужный пароль"," - ", code_in_line)
 
